27a55cfa4a2e6d2b9c9d03f9f00c20809c16071f
sr1k5 <srikanthragh06@gmail.com>
2026-07-07 20:16:50 +0530

    simple /health server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,11 @@
-console.log("hi");
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+
+const app = new Hono();
+
+app.get("/health", (c) => {
+    return c.json({ message: "ok" });
+});
+
+const PORT = 3000;
+serve({ fetch: app.fetch, port: PORT });
